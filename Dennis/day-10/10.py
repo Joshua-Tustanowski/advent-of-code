53350d2cbba726316652b3a49d2057a6d431b80d
Dennis <hello.dennis@hotmail.com>
2020-12-10 21:59:21 +0000

    Saw uttam initiated at 1, quickly ammended and submitted part one :p

--- Dennis/day-10/10.py
+++ Dennis/day-10/10.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
-file = Path('input/test2.txt')
+file = Path('input/data.txt')
 
 data = sorted([int(line) for line in file.open('r')])
 
-one_diffs, three_diffs = 0, 0
+one_diffs, three_diffs = 1, 1
 for i in range(len(data)-1):
     for addition in range(1, 4):
         if data[i] + addition == data[i+1]:
@@ -17,7 +17,7 @@ print(data)
 print('od:', one_diffs, 'td:', three_diffs)
 print(one_diffs * three_diffs)  # Wrong ?
 
-one_diffs, three_diffs = 0, 0
+one_diffs, three_diffs = 1, 1
 for i in range(len(data)-1):
     diff = data[i+1] - data[i]
     if diff == 1:
