f2233b200545e095e1840839ac994be96ed03f51
Josh Tustanowski <josh@fundment.com>
2020-12-13 20:04:52 +0000

    day13

--- Josh/day-13/solution.py
+++ Josh/day-13/solution.py
@@ -14,17 +14,21 @@ def read_and_parse_input(filename: str) -> List[Tuple[str, str]]:
 
 
 def part_one(data: Tuple[int, List[int]]) -> int:
-    timetamp, ids = data
+    timestamp, ids = data
     earliest_time, earliest_id = float('inf'), None
     for id in ids:
         if id < 0:
             continue
-        depart_time = timetamp + id - (timetamp % id)
+        depart_time = timestamp + id - (timestamp % id)
         if depart_time < earliest_time:
             earliest_time = depart_time
             earliest_id = id
-    return (earliest_time - timetamp) * earliest_id
+    return (earliest_time - timestamp) * earliest_id
 
+
+def part_two():
+    'we want the common factors between the buses that are present, CRM?'
+    pass
 
 if __name__ == "__main__":
     content = read_and_parse_input('input.txt')
