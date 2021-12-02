2dc9cbd96c299475458d84bc37a4f72acfe0f25f
Joshua-Tustanowski <josh@fundment.com>
2021-12-02 23:50:50 +0000

    feat: solution to day 1 and renaming of files

---
+++ Josh/solutions_2021/day-1/solution.py
@@ -0,0 +1,46 @@
+from os.path import join, dirname
+from typing import List
+
+from solutions_2021 import DEBUG
+
+current_dir = dirname(__file__)
+
+
+def parse_file(filename: str) -> List[int]:
+    with open(join(current_dir, filename), "r") as fp:
+        contents = fp.read()
+    return [int(val) for val in contents.split("\n")]
+
+
+def part_one(file_name: str) -> int:
+    values = parse_file(file_name)
+    return _get_increasing_count(values)
+
+
+def _get_increasing_count(values: List[int]) -> int:
+    prev, count = values[0], 0
+    for i in range(1, len(values)):
+        if values[i] > prev:
+            count += 1
+        prev = values[i]
+    return count
+
+
+def part_two(file_name: str, window_size: int = 3) -> int:
+    values = parse_file(file_name)
+    return _get_increasing_count(
+        [
+            sum(values[i : i + window_size])
+            for i in range((len(values) - window_size + 1))
+        ]
+    )
+
+
+if __name__ == "__main__":
+    file = "basic-input.txt" if not DEBUG else "sample-input.txt"
+
+    result = part_one(file)
+    print(f"Part one: {result=}")
+
+    result = part_two(file)
+    print(f"Part two: {result=}")
--- Uttam/day4/solve.py
+++ Uttam/day4/solve.py
@@ -4,12 +4,12 @@ import string
 
 
 def load_input_file(filename: str, blocks: bool = True):
-    with open(filename, 'r') as inp:
-        return inp.read().split('\n\n') if blocks else inp.read()
+    with open(filename, "r") as inp:
+        return inp.read().split("\n\n") if blocks else inp.read()
 
 
 def check_attributes1(attributes: set) -> bool:
-    if len(attributes) == 8 or (len(attributes) == 7 and 'cid' not in attributes):
+    if len(attributes) == 8 or (len(attributes) == 7 and "cid" not in attributes):
         return True
     return False
 
@@ -17,19 +17,19 @@ def check_attributes1(attributes: set) -> bool:
 def part1(blocks: List[str]) -> int:
     no_of_valid_passports = 0
     for block in blocks:
-        block = block.replace('\n', ' ')
-        pairs = block.split(' ')
-        attributes = {pair.split(':')[0] for pair in pairs}
+        block = block.replace("\n", " ")
+        pairs = block.split(" ")
+        attributes = {pair.split(":")[0] for pair in pairs}
         if check_attributes1(attributes):
             no_of_valid_passports += 1
     return no_of_valid_passports
 
 
 def test_part1():
-    inp1 = load_input_file('sample.txt')
+    inp1 = load_input_file("sample.txt")
     assert part1(inp1) == 2
 
-    inp = load_input_file('input.txt', blocks=True)
+    inp = load_input_file("input.txt", blocks=True)
     assert part1(inp) == 254
 
 
@@ -39,8 +39,8 @@ def check_attributes2(fields: dict) -> bool:
     valid for automatic validation:
 
     byr (Birth Year) - four digits; at least 1920 and at most 2002.
-    iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-    eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    iyr (Issue Year) - four digits; at least 2010 and at most solutions_2020.
+    eyr (Expiration Year) - four digits; at least solutions_2020 and at most 2030.
     hgt (Height) - a number followed by either cm or in:
         If cm, the number must be at least 150 and at most 193.
         If in, the number must be at least 59 and at most 76.
@@ -54,26 +54,26 @@ def check_attributes2(fields: dict) -> bool:
 
     try:
         # Years
-        assert len(fields['byr']) == 4 and (1920 <= int(fields['byr']) <= 2002)
-        assert len(fields['iyr']) == 4 and (2010 <= int(fields['iyr']) <= 2020)
-        assert len(fields['eyr']) == 4 and (2020 <= int(fields['eyr']) <= 2030)
+        assert len(fields["byr"]) == 4 and (1920 <= int(fields["byr"]) <= 2002)
+        assert len(fields["iyr"]) == 4 and (2010 <= int(fields["iyr"]) <= 2020)
+        assert len(fields["eyr"]) == 4 and (2020 <= int(fields["eyr"]) <= 2030)
 
         # Height
-        height = fields['hgt']
-        if height.endswith('cm'):
+        height = fields["hgt"]
+        if height.endswith("cm"):
             assert 150 <= int(height[:-2]) <= 193
-        elif height.endswith('in'):
+        elif height.endswith("in"):
             assert 59 <= int(height[:-2]) <= 76
         else:
             return False
 
         # Colours
-        assert re.match(r'#[0-9a-f]{6}', fields['hcl'])
-        assert fields['ecl'] in {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'}
+        assert re.match(r"#[0-9a-f]{6}", fields["hcl"])
+        assert fields["ecl"] in {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
 
         # Passport ID
-        assert len(fields['pid']) == 9
-        assert set(fields['pid']) <= set(string.digits)
+        assert len(fields["pid"]) == 9
+        assert set(fields["pid"]) <= set(string.digits)
 
         return True
     except (AssertionError, KeyError):
@@ -84,8 +84,8 @@ def part2(blocks: List[str]) -> int:
     no_of_valid_passports = 0
     for block in blocks:
         attributes = {}
-        for pair in block.replace('\n', ' ').split(' '):
-            attr, val = pair.split(':')
+        for pair in block.replace("\n", " ").split(" "):
+            attr, val = pair.split(":")
             attributes[attr] = val
 
         if check_attributes2(attributes):
@@ -94,11 +94,11 @@ def part2(blocks: List[str]) -> int:
 
 
 def test_part2():
-    invalid_ones = load_input_file('p2_invalid.txt')
+    invalid_ones = load_input_file("p2_invalid.txt")
     assert part2(invalid_ones) == 0
 
-    valid_ones = load_input_file('p2_valid.txt')
+    valid_ones = load_input_file("p2_valid.txt")
     assert part2(valid_ones) == 4
 
-    inp = load_input_file('input.txt')
+    inp = load_input_file("input.txt")
     assert part2(inp) == 184
